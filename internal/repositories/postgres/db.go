fb590356fb8ed1162af92aae4de3350794b13205
dukerupert <logan@fireflysoftware.dev>
2025-05-09 17:46:59 +0000

    consolidated /internal/repositories/postgres files

--- internal/repositories/postgres/db.go
+++ internal/repositories/postgres/db.go
@@ -7,9 +7,40 @@ import (
 	"time"
 
 	"github.com/dukerupert/walking-drum/internal/config"
+	"github.com/dukerupert/walking-drum/internal/repositories/interfaces"
 	_ "github.com/lib/pq" // PostgreSQL driver
 	"github.com/rs/zerolog"
 )
+
+// DB represents a PostgreSQL database connection pool
+type DB struct {
+	*sql.DB
+	*zerolog.Logger
+}
+
+// Repositories holds all repository instances
+type Repositories struct {
+	Product      interfaces.ProductRepository
+	Price        interfaces.PriceRepository
+	Customer     interfaces.CustomerRepository
+	Subscription interfaces.SubscriptionRepository
+}
+
+// NewRepositories initializes all repositories
+func NewRepositories(db *DB, logger *zerolog.Logger) *Repositories {
+	sublogger := logger.With().Str("component", "repository").Logger()
+	return &Repositories{
+		Product:      NewProductRepository(db, sublogger),
+		Price:        NewPriceRepository(db),
+		Customer:     NewCustomerRepository(db),
+		Subscription: NewSubscriptionRepository(db),
+	}
+}
+
+// Close closes the database connection
+func Close(db *DB) error {
+	return db.Close()
+}
 
 // Connect establishes a connection to the PostgreSQL database
 func Connect(dbConfig config.DBConfig, logger *zerolog.Logger) (*DB, error) {
